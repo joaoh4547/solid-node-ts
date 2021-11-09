043c262616723a1c5b1a61a64ebb9b0cb07ecf97
João Henrique <joaohenriquetdj@gmail.com>
2021-11-09 15:18:49 -0300

    add mail trap provider impl

---
+++ src/useCases/CreateUser/CreateUserController.ts
@@ -0,0 +1,19 @@
+import { CreateUserUseCase } from "./CreateUserUseCase";
+import { Request, Response } from "express";
+
+export class CreateUserController {
+  constructor(private createUserUseCase: CreateUserUseCase) {}
+
+  async handle(req: Request, res: Response): Promise<Response> {
+    const { name, email, password } = req.body;
+
+    try {
+      await this.createUserUseCase.execute({ name, email, password });
+      return res.status(201).send();
+    } catch (err) {
+      return res
+        .status(400)
+        .json({ message: err.message || "Unexpected Error" });
+    }
+  }
+}
--- src/useCases/CreateUser/CreateUserUseCase.ts
+++ src/useCases/CreateUser/CreateUserUseCase.ts
@@ -1,3 +1,34 @@
+import { IMailProvider } from "./../../providers/IMailProvider";
+import { ICreateUserRequestDTO } from "./CreateUserDTO";
+import { IUsersRepository } from "./../../repositories/IUserRepository";
+import { User } from "../../entities/User";
 export class CreateUserUseCase {
-  async execute() {}
+  constructor(
+    private usersRepository: IUsersRepository,
+    private mailProvider: IMailProvider
+  ) {}
+
+  async execute(data: ICreateUserRequestDTO) {
+    const userAlreadyExists = await this.usersRepository.findByEmail(
+      data.email
+    );
+
+    if (userAlreadyExists) {
+      throw new Error("User already exists");
+    }
+    const user = new User(data);
+    await this.usersRepository.save(user);
+    this.mailProvider.sendMail({
+      to: {
+        name: data.name,
+        email: data.email,
+      },
+      from: {
+        name: "Equipe do app",
+        email: "equipe@meuapp.com",
+      },
+      subject: "Seja bem vindo à plataforma",
+      body: "<p>Você já pode fazer login na nossa aplicação</p>",
+    });
+  }
 }
